0d16d1750f529d5a0fe3af909a59d7b096511d05
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-16 13:38:33 +0530

    HashTable Program Added

---
+++ Day15/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Day15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To ");
+            hash.Add("1", "be ");
+            hash.Add("2", "or ");
+            hash.Add("3", "not ");
+            hash.Add("4", "to ");
+            hash.Add("5", "be ");
+
+            Console.Write(hash.GetV("0"));
+            Console.Write(hash.GetV("1"));
+            Console.Write(hash.GetV("2"));
+            Console.Write(hash.GetV("3"));
+            Console.Write(hash.GetV("4"));
+            Console.Write(hash.GetV("5"));
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
